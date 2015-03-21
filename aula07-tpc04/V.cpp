9387f175ddf7a7ceaa6d71992f42fc6344b58f46
Jose Simao <jsimao@cc.isel.ipl.pt>
2015-03-21 00:59:20 +0000

    Aula 07: Update to V.cpp to display address of 'this'

--- aula07-tpc04/V.cpp
+++ aula07-tpc04/V.cpp
@@ -9,16 +9,20 @@ using namespace System;
 value struct V {
 private:
 	int x;
+	int y;
 public:
 	V(int x){
 		this->x = x;
+		this->y = 10;
 	}
 	int Print() {
 		return printf("V=%d\n", x);
 	}
 
 	void Address(){
-		printf("address of field x: %p\n", &x);
+		printf("address of field this: %p\n", this);
+		printf("address of field x: %p\n", &(this->x));
+		printf("address of field y: %p\n", &(this->y));
 	}
 };
 
