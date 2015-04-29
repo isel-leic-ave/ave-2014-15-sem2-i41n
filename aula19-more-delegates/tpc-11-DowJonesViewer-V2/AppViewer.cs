bfca7c0cc04515b00f7cc63e86257e22b2a2846c
Jose Simao <jsimao@cc.isel.ipl.pt>
2015-04-29 11:40:00 +0100

    Aula 19: TPC 11

---
+++ aula19-more-delegates/tpc-11-DowJonesViewer-V2/AppViewer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+    
+class AppViewer
+{
+    public static void ViewOnConsole(string title, string uri, DateTime when)
+    {
+        Console.WriteLine("{0} ({1}): {2}", title, uri, when);
+    }
+
+    static void Main(string[] args)
+    {
+        DowjonesEventWrapper news = new DowjonesEventWrapper();
+        news.AddHandler(new DowjonesEventHandler(ViewOnConsole));
+        news.AddHandler( (t, uri, dt) => MessageBox.Show(String.Format("{0} ({1}): {2}", t, uri, dt)));
+        news.Pull();
+        news.RemoveHandler(new DowjonesEventHandler(ViewOnConsole));
+        news.Pull();
+    }
+}
+    
