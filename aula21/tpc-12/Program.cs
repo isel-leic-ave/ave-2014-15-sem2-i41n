a845ca4231f9c21748f0442dcb92bee6a6e1d4db
Jose Simao <jsimao@cc.isel.ipl.pt>
2015-05-05 21:10:32 +0100

    Aula 21: code for TPC-12

---
+++ aula21/tpc-12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpc_12
+{
+    class TargetMethods
+    {
+        public delegate void Action(int x);
+        public delegate void OtherAction(int x);
+
+        public static void DoAction(Action a, int i) { a(i); }
+
+        public static void SomeAction(int i) { Console.WriteLine(i); }
+
+        public static void Main()
+        {
+            OtherAction o = SomeAction;
+            DoAction(SomeAction, 10);
+            DoAction(o, 10);
+
+            Action a0 = delegate(int x) { Console.Beep(2000, x); };
+            Action a1 = SomeAction;
+            Action a2 = (Action) Delegate.Combine(a1, a0);
+            Action a3 = (y) => { Console.WriteLine("*" + y + "*"); };
+            
+            a3 += a2;
+            a3(1000);
+
+            a3 -= new Action(SomeAction);
+            a3(1100);
+        }
+    }
+}
