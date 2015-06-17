3b78537b8e94f6accb479e358fbdd2c0376b12d1
Jose Simao <jsimao@cc.isel.ipl.pt>
2015-06-17 11:06:17 +0100

    aula 38: exercício MakeNotifier

---
+++ aula38-exercícios/DelegateMaxCalls/MakeNotifiers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateMaxCalls
+{
+
+    public class NotifyArgs { }
+
+    public delegate void NotifyHandler(NotifyArgs arg);
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class MaxCallsAttribute : Attribute
+    {
+        private int maxCalls = Int32.MaxValue;
+        public MaxCallsAttribute(int max) { maxCalls = max; }
+        public int MaxCalls
+        {
+            get { return maxCalls; }
+            set { maxCalls = value; }
+        }
+    }
+
+
+    public class Aula38
+    {
+        private class Pair<T, W>
+        {
+            public T Item1 { get; set; }
+            public W Item2 { get; set; }
+        }
+        public static NotifyHandler MakeNotifier(Type klass)
+        {
+            Object _this = null;
+            List<Pair<int, NotifyHandler>> maxCallsRecord = new List<Pair<int, NotifyHandler>>();
+            foreach (MethodInfo mi in klass.GetMethods())
+            {
+                MaxCallsAttribute attr = (MaxCallsAttribute)
+                    Attribute.GetCustomAttribute(mi, typeof(MaxCallsAttribute));
+                if (attr != null
+                    && mi.ReturnType == typeof(void)
+                    && mi.GetParameters().Length == 1
+                    && mi.GetParameters()[0].ParameterType == typeof(NotifyArgs))
+                {
+                    NotifyHandler d;
+                    if (mi.IsStatic)
+                    {
+                        d = (NotifyHandler)Delegate.CreateDelegate(
+                            typeof(NotifyHandler),
+                            mi);
+                    }
+                    else
+                    {
+                        if (_this == null)
+                        {
+                            _this = Activator.CreateInstance(klass);
+                        }
+                        d = (NotifyHandler)Delegate.CreateDelegate(
+                            typeof(NotifyHandler), _this, mi);
+                    }
+                    Pair<int, NotifyHandler> pair = new Pair<int, NotifyHandler>();
+                    pair.Item1 = attr.MaxCalls;
+                    pair.Item2 = d;
+                    maxCallsRecord.Add(pair);
+                }
+            }
+            return args =>
+            {
+                foreach (Pair<int, NotifyHandler> p in maxCallsRecord)
+                {
+                    if (p.Item1 > 0)
+                    {
+                        p.Item1--;
+                        p.Item2(args);
+                    }
+                }
+            };
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Demo
+            NotifyHandler h = MakeNotifier(typeof(Handlers));
+            h(null); // M1, M2
+            h(null); // M1
+            h(null); // ()
+        }
+    }
+}
---
+++ aula38-exercícios/DelegateMaxCalls/TestHandlers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateMaxCalls
+{
+    class Handlers
+    {
+        [MaxCalls(2)]
+        static public void M1(NotifyArgs arg) { Console.WriteLine("M1"); }
+        [MaxCalls(1)]
+        static public void M2(NotifyArgs arg) { Console.WriteLine("M2(NotifyArgs)"); }
+        [MaxCalls(7)]
+        public void M2(string arg)
+        {
+            // nao é compativel, logo nao é chamado
+            Console.WriteLine("M2(string)");
+        }
+        public void M4(NotifyArgs arg)
+        {
+            // nao é anotado, logo não é chamado
+        }
+    }
+}
